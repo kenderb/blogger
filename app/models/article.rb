dda81ae9427feff68ed3ae9e62b7509b13ca3d40
julian3493 <julianramosarevalo@gmail.com>
2020-11-17 11:24:30 -0500

    add attachment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings, dependent: :destroy
+  has_many :attachments, dependent: :destroy
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   def tag_list
